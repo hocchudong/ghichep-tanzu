1cae69e056210a505dda9605742c09b72144bfeb
Tô Thành Công <tcvn1985@gmail.com>
2021-12-24 17:45:31 +0700

    cap nhat file cai

---
+++ scripts/00_tce_build_variables.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -eu
+
+#----------------------------------------------------------------------------------------------------
+# Tanzu Community Edition - Build Variable Definition
+# juliusn - Sun Dec  5 08:48:39 EST 2021 - first version
+# https://github.com/jmnicolescu/packer-tce-ubuntu/blob/main/scripts/00-tce-build-variables.sh
+#----------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------
+# Modify: Cong TO - HCD
+#----------------------------------------------------------------------------------------------------
+
+export TCE_VERSION="0.9.1"
+export K8S_VERSION="1.20.1"
+export KIND_VERSION="0.11.1"
+export OCTANT_VERSION="0.25.0"
+
+export MGMT_CLUSTER_NAME="tce-mgnt"
+export WKLD_CLUSTER_NAME="tc-wl01"
+export MGMT_VSPHERE_CONTROL_PLANE_ENDPOINT="192.168.20.85"
+export WKLD_VSPHERE_CONTROL_PLANE_ENDPOINT="192.168.20.86"
+export DEPLOY_TKG_ON_VSPHERE7="true" 
+
+export VSPHERE_DATACENTER="HCD-DC01"
+export VSPHERE_CLUSTER="HCD-DC01"
+export VSPHERE_DATASTORE="datastore4-r620"
+export VSPHERE_NETWORK_SWITCH="infra-vds-01"
+export VSPHERE_NETWORK_PG="dvpg-vlan-20"
+export VSPHERE_TLS_THUMBPRINT="91:CC:21:D7:2E:9B:AB:A2:9F:9C:EE:77:19:59:A0:93:60:12:47:BC"
+export VSPHERE_SERVER: vc.hcdlab.local
+export VSPHERE_USERNAME: administrator@vsphere.local
+export VSPHERE_SSH_KEY=`cat ${HOME}/.ssh/id_rsa.pub`
+export VSPHERE_FOLDER="tanzu-folder"
+export VSPHERE_RESOURCE_POOL="HCD-Tanzu-Resources"
+
+# K8s node VM settings for Photon OS
+# export OVA_VM_NAME="photon-3-kube-v1.21.2+vmware.1"
+# export OVA_FILE="${HOME}/ova/photon-3-kube-v1.21.2+vmware.1-tkg.2-12816990095845873721.ova"
+# export OVA_JSON_FILE="${HOME}/ova/kubernetes-node-ova-specs.json"
+# export NODE_OS_NAME="photon"
+# export NODE_OS_VERSION="3"
+
+Kubernetes node OS VM settings for Ubuntu OS
+# export OVA_VM_NAME="ubuntu-2004-kube-v1.21.2+vmware.1"
+# export OVA_FILE="${HOME}/ova/ubuntu-2004-kube-v1.21.2+vmware.1-tkg.1-7832907791984498322.ova"
+# export OVA_JSON_FILE="${HOME}/ova/kubernetes-node-ova-specs.json"
+# export NODE_OS_NAME="ubuntu"
+# export NODE_OS_VERSION="20.04"
+
+# export GOVC_URL="https://$(pass provider_vcenter_hostname)"
+# export GOVC_USERNAME=$(pass provider_vcenter_username)
+# export GOVC_PASSWORD=$(pass provider_vcenter_password)
+# export GOVC_INSECURE=true
+# export GOVC_DATASTORE="${VSPHERE_DATASTORE}"
+# export GOVC_NETWORK="${VSPHERE_NETWORK_PG}"
+
+# Ubuntu
+export MY_IP_ADDRESS=`ifconfig eth0 | grep '192.168.' | awk '{ print $2}'`
+export MY_DOMAIN_NAME="hcdlab.local"
+
+
+export METALLB_VIP_RANGE="192.168.20.90-192.168.20.95"
---
+++ scripts/01_tce_bootstraps.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+## CongTO - HCD.
+
+###########################################
+# Funtion
+###########################################
+function echolog() {
+  local line
+
+  echo ""
+  for line in "$@"
+  do
+    echo ">>>>> $line"
+  done
+  echo ""
+}
+
+source ${HOME}/scripts/00-tce-build-variables.sh
+
+# Setup basic bootstraps server.
+echolog "Setup basic bootstraps server"
+
+apt update -y
+
+hostnamectl set-hostname tcebootstrap
+echo "Updating /etc/hosts file."
+echo "${MY_IP_ADDRESS} ${SHORT_HOST}.${MY_DOMAIN_NAME} ${SHORT_HOST}" >>/etc/hosts
+
+
+
+timedatectl set-timezone Asia/Ho_Chi_Minh
+
+apt install chrony -y
+systemctl start chrony
+systemctl enable chrony
+
+# Setup docker on bootstraps server.
+echolog "Setup docker on bootstraps server"
+
+apt remove docker docker-engine docker.io containerd runc
+apt install -y ca-certificates curl gnupg lsb-release jq
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt install docker-ce docker-ce-cli containerd.io -y
+usermod -aG docker $USER
+
+systemctl enable docker
+systemctl start docker
+
+
+# Download and Install Tanzu Community Edition
+cd ~
+
+echolog "Download and Install Tanzu Community Edition release ${TCE_VERSION}"
+curl -LO https://github.com/vmware-tanzu/community-edition/releases/download/v${TCE_VERSION}/tce-linux-amd64-v${TCE_VERSION}.tar.gz
+tar xzvf tce-linux-amd64-v${TCE_VERSION}.tar.gz 
+cd tce-linux-amd64-v${TCE_VERSION}
+
+export ALLOW_INSTALL_AS_ROOT=true
+./uninstall.sh
+./install.sh
+
+tanzu version
+tanzu plugin list
+
+cd ~
+
+echo "Download and install kubectl version ${K8S_VERSION}"
+curl -LO https://dl.k8s.io/release/v${K8S_VERSION}/bin/linux/amd64/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Download and install kind version ${KIND_VERSION}"
+curl -LO https://kind.sigs.k8s.io/dl/v${KIND_VERSION}/kind-linux-amd64
+install -o root -g root -m 0755 kind-linux-amd64 /usr/local/bin/kind
+
+echo "Download and install Octant version ${OCTANT_VERSION}"
+curl -LO https://github.com/vmware-tanzu/octant/releases/download/v${OCTANT_VERSION}/octant_${OCTANT_VERSION}_Linux-64bit.tar.gz
+tar xzvf octant_${OCTANT_VERSION}_Linux-64bit.tar.gz
+install -o root -g root -m 0755 octant_${OCTANT_VERSION}_Linux-64bit/octant /usr/local/bin/octant
+
+# Carvel
+curl -L https://carvel.dev/install.sh -o install-carvel.sh
+sudo bash install-carvel.sh
+rm install-carvel.sh
+
+# Download and Install Helm 3
+
+echo "Download and install Helm 3"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
