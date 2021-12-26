55bab3f9353b901a133e94ce520e944fe802ef5b
Tô Thành Công <tcvn1985@gmail.com>
2021-12-26 16:00:44 +0700

    cap nhat file

--- scripts/tkg_01_bootstraps.sh
+++ scripts/tkg_01_bootstraps.sh
@@ -143,8 +143,8 @@ sudo tanzu completion bash | sudo tee /etc/bash_completion.d/tanzu > /dev/null
 
 type _init_completion
 
-echo 'source <(kubectl completion bash)' >> ~/.bashrc > /dev/null
-echo 'source <(tanzu completion bash)' >> ~/.bashrc > /dev/null
+echo 'source <(kubectl completion bash)' >> ~/.bashrc 
+echo 'source <(tanzu completion bash)' >> ~/.bashrc 
 
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
 tanzu completion bash | sudo tee /etc/bash_completion.d/tanzu
